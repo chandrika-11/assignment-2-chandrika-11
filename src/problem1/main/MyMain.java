d251e5f78d5fabff85a254276905a3d59ad294e2
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 12:59:01 +0530

    complete main method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,6 +8,7 @@ package problem1.main;
 // executable class
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 import java.util.Scanner;
 
@@ -15,7 +16,16 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MyBinarySearchTree bst = new MyBinarySearchTree();
-
+        System.out.println("Enter number of nodes=");
+        int n = scanner.nextInt();
+        while (n > 0) {
+            System.out.println("Enter data=");
+            int element = scanner.nextInt();
+            TreeNode node = new TreeNode();
+            node.setData(element);
+            bst.insert(node);
+        }
+        System.out.println(bst.showLeft(bst.getRoot(), false));
     }
 
 }
