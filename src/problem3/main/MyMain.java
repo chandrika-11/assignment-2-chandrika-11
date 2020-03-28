9643240f573eb949fa07a5ed5ec9cfcd4027e2b4
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 15:45:35 +0530

    create object

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,10 +6,16 @@
  */
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyPriorityQueue priorityQueue = new MyPriorityQueue();
 
     }
 }
