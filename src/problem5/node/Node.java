202b8eafbab944fe19fe454c866e95d0568a27df
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 13:56:31 +0530

    add getter and setter method

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -11,4 +11,20 @@ import problem5.student.Student;
 public class Node {
     private Student student;
     private Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
