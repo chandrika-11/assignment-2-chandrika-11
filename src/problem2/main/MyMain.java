6714f859c4e57c0e9f91541c171f9c4fbf0ba221
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 13:38:03 +0530

    update main method of problem2

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,6 +7,7 @@
 package problem2.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 import java.util.Scanner;
 
@@ -16,9 +17,19 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MyBinarySearchTree bst = new MyBinarySearchTree();
-        System.out.print("Enter number of nodes=");
+        System.out.println("Enter number of nodes=");
         int n = scanner.nextInt();
+        while (n-- > 0) {
+            System.out.println("Enter data=");
+            int element = scanner.nextInt();
+            TreeNode node = new TreeNode();
+            node.setData(element);
+            bst.insert(node);
+        }
+        System.out.println("----PreOrder Traversal out-----");
+        bst.showTreeData(bst.getRoot());
+        System.out.println("----PostOrder Traversal");
+        bst.postOrderTraversal(bst.getRoot());
+        System.out.println("First element is valid but Second is not");
     }
-
-
 }
