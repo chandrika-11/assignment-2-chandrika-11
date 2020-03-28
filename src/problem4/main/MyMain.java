2ed5c21cc4edd39fffbe0aa9b6669c06caaef941
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 16:26:12 +0530

    complete problem4

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -6,6 +6,12 @@
  */
 package problem4.main;
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+import problem4.node.Node;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // executable class
@@ -13,6 +19,32 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        MyQueue queue = new MyQueue();
+        System.out.println("Enter number of nodes");
+        int nodes = scanner.nextInt();
+        while (nodes-- > 0) {
+            System.out.println("Enter data");
+            int data = scanner.nextInt();
+            TreeNode treeNode = new TreeNode();
+            treeNode.setData(data);
+            bst.insert(treeNode);
+        }
 
+        ArrayList<Integer> arrayList = bst.getPreOrderList();
+        for (int i = 1; i < arrayList.size(); i++) {
+            Node node = new Node();
+            node.setData(arrayList.get(i));
+            queue.enQueue(node);
+        }
+        System.out.println("Pre Order successor of all nodes are");
+        int i = 0;
+        Node node = queue.deQueue();
+        while (node != null) {
+            System.out.println("Key" + arrayList.get(i) + "Successor" + node.getData());
+            node = queue.deQueue();
+            i++;
+        }
+        System.out.println("Key" + arrayList.get(i) + "" + "Not Available");
     }
 }
