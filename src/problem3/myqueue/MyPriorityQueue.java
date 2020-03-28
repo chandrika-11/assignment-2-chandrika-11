f6e05521e7c2bf1df6bc7ae8846aa663a40f7795
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 15:26:35 +0530

    getter and setter

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,17 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
+import problem3.node.Node;
 
 public class MyPriorityQueue {
+    private Node front;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
 }
