61f5f1aba591d2fa83354bb2067ca0b0115ba8b0
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 13:47:13 +0530

    add getter and setter method

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -11,4 +11,27 @@ public class Student {
     private int rollNumber;
     private int backLogCount;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getBackLogCount() {
+        return backLogCount;
+    }
+
+    public void setBackLogCount(int backLogCount) {
+        this.backLogCount = backLogCount;
+    }
 }
