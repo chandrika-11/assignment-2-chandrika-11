4395b11d7a299060d0847f83d30a959144413659
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 15:55:41 +0530

    update main method

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,6 +7,8 @@
 package problem3.main;
 
 import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
 
 import java.util.Scanner;
 
@@ -16,6 +18,25 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MyPriorityQueue priorityQueue = new MyPriorityQueue();
-
+        System.out.println("Enter number of nodes");
+        int nodes = scanner.nextInt();
+        while (nodes-- > 0) {
+            Student student = new Student();
+            System.out.println("Enter name");
+            String name = scanner.next().trim();
+            System.out.println("Enter roll number");
+            int roll = scanner.nextInt();
+            student.setName(name);
+            student.setRollNumber(roll);
+            Node node = new Node();
+            node.setStudent(student);
+            priorityQueue.enQueue(node);
+        }
+        System.out.println("Display Queue");
+        Node node = priorityQueue.deQueue();
+        while (node != null) {
+            System.out.println(node);
+            node = priorityQueue.deQueue();
+        }
     }
 }
