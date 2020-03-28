ef8d94925a2a19f24d8f601e28315f97b29fdf5d
chandrika-11 <chandrika.singhal_cs18@gla.ac.in>
2020-03-28 13:55:05 +0530

    add private variables

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,10 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private Student student;
+    private Node next;
 }
